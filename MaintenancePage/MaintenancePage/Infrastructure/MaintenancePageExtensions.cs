bf4e51add7e63c5f1273a57fa196dd48dcc21cad
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2019-11-15 13:39:34 +0100

    Cleaning up unnecessary usings.

--- MaintenancePage/MaintenancePage/Infrastructure/MaintenancePageExtensions.cs
+++ MaintenancePage/MaintenancePage/Infrastructure/MaintenancePageExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MaintenancePage.Infrastructure
 {
