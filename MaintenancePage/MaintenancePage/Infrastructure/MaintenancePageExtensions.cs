f8017c0a9af8ef75651a98312f805abe3a4d8cda
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2019-11-15 11:46:09 +0100

    Adding basic classes and interfaces for maintenance page.

---
+++ MaintenancePage/MaintenancePage/Infrastructure/MaintenancePageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaintenancePage.Infrastructure
+{
+    public class MaintenancePageExtensions
+    {
+    }
+}
